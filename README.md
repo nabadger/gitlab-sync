b1852c8dabb2432bcf1ccbc46a261f1d567ac79a
nabadger <>
2018-09-29 20:39:22 +0100

    initial

---
+++ README.md
@@ -0,0 +1,48 @@
+# Gitlab Sync
+
+This is a helper python program which synchronizes remote Gitlab repo(s) with support for group / sub-groups.
+
+The program accepts a single top-level `group-id`, which it will use 
+to traverse through the sub-groups to perform a`git [clone|pull]` command using ssh.
+
+## Reqiures
+
+* pipenv (for python)
+* gitlab api token
+* gitlab configured with ssh-key
+
+## Install
+
+```
+pipenv install
+```
+
+## Run
+```
+pipenv run python src/gitlab-sync.py --help
+
+usage: gitlab-sync.py [-h] [--group-id GROUP_ID]
+                      [--local-repo-path LOCAL_REPO_PATH]
+                      [--gitlab-token GITLAB_TOKEN] [--dry-run] [--no-dry-run]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --group-id GROUP_ID   group id
+  --local-repo-path LOCAL_REPO_PATH
+                        local repo path (clone under this dir)
+  --gitlab-token GITLAB_TOKEN
+                        gitlab private token
+  --dry-run             dry-run (do not do git commands)
+  --no-dry-run          no-dry-run (do git commands)
+```
+
+## Example
+
+```
+pipenv run python src/gitlab-sync.py \
+	--group-id <numeric-gitlab-group-id> \
+	--local-repo-path /tmp/mycheckouts \
+	--gitlab-token <gitlab-api-token> \
+	--no-dry-run 
+
+```
---
+++ src/gitlab-sync.py
@@ -0,0 +1,97 @@
+"""
+This is a helper script to git clone/pull gitlab-projects which
+are in the specified group (includes all children).
+"""
+import gitlab
+import os
+import sys
+import argparse
+from git import Repo
+from colorama import Fore, Style
+
+
+def clone_projects(local_repo_path, projects, dry_run):
+    """
+    Clone/pull all projects in the group
+    """
+
+    for project in projects:
+        local_repo_fullpath = os.path.join(
+            local_repo_path, project.path_with_namespace)
+        if os.path.exists(local_repo_fullpath):
+            os.chdir(local_repo_fullpath)
+            print(f'\t{Fore.CYAN}Pulling{Style.RESET_ALL}: %s' %
+                  (local_repo_fullpath))
+            if dry_run:
+                print(f'\t\t{Fore.YELLOW}Skipping (dry-run){Style.RESET_ALL}')
+            else:
+                repo = Repo(local_repo_fullpath)
+                o = repo.remotes.origin
+                o.pull()
+        else:
+            print(f'\t{Fore.GREEN}Cloning{Style.RESET_ALL}: %s' %
+                  (local_repo_fullpath))
+            if dry_run:
+                print(f'\t\t{Fore.YELLOW}Skipping (dry-run){Style.RESET_ALL}')
+            else:
+                Repo.clone_from(project.ssh_url_to_repo, local_repo_fullpath)
+
+
+def sync_gitlab_repos(gl, args):
+    """
+    Get all groups / subgroups which we should sync
+    """
+
+    def get_groups(group, group_name):
+        print(f'Group {Fore.YELLOW}%s (id: %s){Style.RESET_ALL}' %
+              (group_name, group.id))
+
+        projects = group.projects.list()
+        clone_projects(args.local_repo_path, projects, args.dry_run)
+
+        subgroups = group.subgroups.list(all=True)
+        for subgroup in subgroups:
+            real_g = gl.groups.get(subgroup.id, lazy=True)
+            get_groups(real_g, subgroup.name)
+
+    top_group = gl.groups.get(args.group_id)
+    get_groups(top_group, top_group.name)
+
+
+def main(args):
+    """
+    Init settings and start
+    """
+
+    # Grab gitlab token from environment and exit if not set
+    gitlab_private_token = \
+        os.environ.get('GITLAB_PRIVATE_TOKEN') or \
+        args.gitlab_token
+
+    if not gitlab_private_token:
+        print('Please set GITLAB_PRIVATE_TOKEN in env')
+        sys.exit(1)
+
+    gl = gitlab.Gitlab('https://gitlab.com',
+                       private_token=gitlab_private_token)
+    gl.auth()
+
+    sync_gitlab_repos(gl, args)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--group-id", help="group id")
+    parser.add_argument("--local-repo-path",
+                        help="local repo path (clone under this dir)")
+    parser.add_argument("--gitlab-token", help="gitlab private token")
+    parser.add_argument("--dry-run", dest="dry_run",
+                        action='store_true', default=True,
+                        help="dry-run (do not do git commands)")
+    parser.add_argument("--no-dry-run", dest="dry_run",
+                        action='store_false', default=False,
+                        help="no-dry-run (do git commands)")
+
+    args = parser.parse_args()
+    main(args)
